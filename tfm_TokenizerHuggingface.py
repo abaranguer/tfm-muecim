369e2644d37a76f7f081e352dfe9ae68ca023ecc
abaranguer <abaranguer@gmail.com>
2024-12-26 22:05:14 +0100

    migració a Huggingface sobre colab

--- tfm_TokenizerHuggingface.py
+++ tfm_TokenizerHuggingface.py
@@ -1,18 +1,15 @@
 import torch
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModel
 from datasets import load_dataset
 
 # https://huggingface.co/docs/transformers/en/training
 
-
-class Tokenizator(self):
-    def __init__():
+class CustomTokenizer:
+    def __init__(self):
         print('Building tokens...')
 
         tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-        dataset = dataset.map(lambda examples: tokenizer(examples["text"]), batched=True)
-
-
+        model = AutoModel.from_pretrained("bert-base-cased")
 
         print('Tokenization done.')
 
