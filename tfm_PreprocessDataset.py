3c82b92e2e96ae8b7d651087213932e9b6e7a2f0
Albert Baranguer i Codina <abaranguer@gmail.com>
2024-12-29 23:58:42 +0000

    construir els tensors d'entrada del dataset complet (basic)

--- tfm_PreprocessDataset.py
+++ tfm_PreprocessDataset.py
@@ -1,13 +1,13 @@
-import re
 import os
 import json
 import numpy as np
-import pandas as pd
-from tfm_BERTutils import BERTTokenizer
+import torch
+from tfm_BERTUtils import BERTTokenizer
 
 # itera pel sets (dev, test, train)
 # convertint el text del main_body de cada cada json
-# en un vector de 768 elements,
+# en un vector de 768 elements (768 és el nombre de input-features
+# de la input layer del BertModel de la llibreria Huggingface)   
 # descartant els tokens sobrants (cropping)
 # o completant amb zeros (padding)
 
@@ -16,6 +16,7 @@ class BasicPreprocesssor:
         self.folderBase = 'data/datasets/EURLEX57K/'
         self.folders = ['dev','test','train']
         tknzr = BERTTokenizer()
+        fdErr = open(f'{self.folderBase}/errors.txt', 'w')
         
         for folder in self.folders:
             print(f'Iterating through folder {folder}')
@@ -24,31 +25,55 @@ class BasicPreprocesssor:
                 for name in fullnames:
                     fullName = f'{fullNameFolder}/{name}'
                     fp = open(fullName, 'r')
-                    jsonFile = json.load(fp)
-                    mainBody = jsonFile.get('main_body')
+                    try:
+                        jsonFile = json.load(fp)
+                        mainBody = jsonFile.get('main_body')
+                    except Exception as ex:
+                        fdErr.write(f'full name: {fullName}')
+                        mainBody = ''
                     fp.close()
 
-                    tokens = np.array([])
+                    self.tokens = np.array([])
                     for part in mainBody:
                         lines = part.split('\n')
                         for line in lines:
-                            toks = tknzr.tokenizer.tokenize(line)
+                            seq = line[:512]
+                            toks = tknzr.tokenizer.tokenize(seq)
                             ids = tknzr.tokenizer.convert_tokens_to_ids(toks)
                             idsNp = np.array(ids)
-                            self.tokens = np.concatenate((self.tokens, idsNp))
+                            self.concatenate(idsNp)
 
-                    if len(self.tokens) < 768:
-                        self.tokens = self.padWithZeros()
-
-                    if len(self.tokens) > 768:
-                        self.tokens = self.tokens[0:768]
+                    self.adjustlength()
 
                     self.save(fullName)
 
-    def padWithzeros(self):
-        # TODO
-        None
+        fdErr.close()
+
+    def adjustlength(self):
+        if self.tokens.size < 768:
+            self.padWithZeros()
+
+        if self.tokens.size > 768:
+            self.tokens = self.tokens[0:768]
+
+    def padWithZeros(self):
+        lenPadArray = 768 - self.tokens.size
+        padArray = np.zeros(lenPadArray, dtype=int)
+        self.tokens = np.concatenate((self.tokens, padArray))
+
+
+    def concatenate(self, idsNp):
+        if self.tokens.size > 0:
+            self.tokens = np.concatenate((self.tokens, idsNp))
+        else:
+            self.tokens = idsNp
 
     def save(self, fileName):
-        # TODO
-        None
+        vectorFileName = f'{fileName}.tensor'
+        tokensTensor = torch.from_numpy(self.tokens)
+        torch.save(tokensTensor, vectorFileName)
+
+if __name__ == '__main__':
+    print('Init')
+    BasicPreprocesssor()
+    print('Done!')
