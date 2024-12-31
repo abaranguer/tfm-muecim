08b74ec1bb76f157a6c395e354d8c8ffb39c003b
Albert Baranguer i Codina <abaranguer@gmail.com>
2024-12-31 11:49:44 +0000

    afegir index general

--- tfm_CreateFileIndices.py
+++ tfm_CreateFileIndices.py
@@ -4,7 +4,10 @@ class FilesIndexCreator:
     def __init__(self):
         folderBase = 'data/datasets/EURLEX57K'
         folders = ['dev','test','train']
-        
+
+        ixg = 0
+        filesIndexFileName = f'{folderBase}/FilesIndex.txt'
+        fdg = open(filesIndexFileName, 'w')
         for folder in folders:
             filesIndexFileName = f'{folderBase}/{folder}FilesIndex.txt'
             fd = open(filesIndexFileName, 'w')
@@ -15,12 +18,15 @@ class FilesIndexCreator:
                 for name in fullnames:
                     fullName = f'{fullNameFolder}/{name}'
                     fd.write(f'{ix},{fullName}\n')
+                    fdg.write(f'{ixg},{fullName}\n')
                     ix = ix + 1
+                    ixg = ixg + 1
                     if (ix % 1000) == 0:
                         print(f'Processing index {ix}')
 
             fd.close()
 
+        fdg.close()
         print('Done!')
 
 if __name__ == '__main__':
