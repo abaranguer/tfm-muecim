fb441a315aab1e297e6f2ce315d893e911a9db33
Albert Baranguer i Codina <abaranguer@gmail.com>
2025-01-01 02:32:37 +0000

    classe bàsica dataset per al dataset loader

---
+++ tfm_BERTTokenizer.py
@@ -0,0 +1,51 @@
+import os
+import json
+import numpy as np
+import torch
+from transformers import AutoTokenizer
+
+# convertint el text del main_body de cada cada json
+# en un vector de 768 elements (768 és el nombre de input-features
+# de la input layer del BertModel de la llibreria Huggingface)   
+# descartant els tokens sobrants (cropping)
+# o completant amb zeros (padding)
+
+class BERTTokenizer:
+    def __init__(self):
+        self.reset()
+        print('Loading bert-base-cased tokenizer.')
+        self.tknzr = AutoTokenizer.from_pretrained('bert-base-cased')
+
+    def reset(self):
+        self.lines = []
+        self.tokens = np.array([])
+
+    def tokenize(self, lines):
+        self.lines = lines
+        for line in self.lines:
+            seq = line[:512]
+            toks = self.tknzr.tokenize(seq)
+            ids = self.tknzr.convert_tokens_to_ids(toks)
+            idsNp = np.array(ids)
+            self.concatenate(idsNp)
+            self.adjustlength()
+            tokensTensor = torch.from_numpy(self.tokens)
+            return tokensTensor
+
+    def adjustlength(self):
+        if self.tokens.size < 768:
+            self.padWithZeros()
+
+        if self.tokens.size > 768:
+            self.tokens = self.tokens[0:768]
+
+    def padWithZeros(self):
+        lenPadArray = 768 - self.tokens.size
+        padArray = np.zeros(lenPadArray, dtype=int)
+        self.tokens = np.concatenate((self.tokens, padArray))
+
+    def concatenate(self, idsNp):
+        if self.tokens.size > 0:
+            self.tokens = np.concatenate((self.tokens, idsNp))
+        else:
+            self.tokens = idsNp
--- tfm_EURLEX57KDataset.py
+++ tfm_EURLEX57KDataset.py
@@ -1,15 +1,102 @@
 import torch
+import json
+import numpy as np
+from tfm_BERTTokenizer import BERTTokenizer
+
 
 class EURLEX57KDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
+    def __init__(self, fileIndex):
+        self.fileIndex = fileIndex
+        self.listFiles = {}
+        self.bertTknzr = BERTTokenizer()
+        self.loadFiles()
         
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
+        fileName = self.listFiles[idx]
+        fd = open(fileName, 'r')
+        jsonObj = json.load(fd)
+        fd.close()
+        item =  self.toTensor(jsonObj)
         return item
     
     def __len__(self):
-        return len(self.labels)
+        return len(self.listFiles)
+
+    def loadFiles(self):
+        fd = open(self.fileIndex, "r")
+        lines = fd.readlines()
+        for line in lines:
+            tokens = line.split(',')
+            key = tokens[0]
+            value = tokens[1].strip()
+            self.listFiles[int(key)] = value
+
+        fd.close()
+
+    def toTensor(self, jsonObj):
+        labels = jsonObj.get('concepts')
+        title = jsonObj.get('title')
+        header = jsonObj.get('header')
+        recitals = jsonObj.get('recitals')
+        mainBody = jsonObj.get('main_body')
+        attachments = jsonObj.get('attachments')
+        
+        rawFullText = [title, header, recitals, mainBody, attachments]
+                
+        rawFlattenedData = self.getData(rawFullText)
+        filteredData =  self.filterData(rawFlattenedData)
+        item = {}
+        item['data'] =  self.dataToTensor(filteredData)
+        item['labels'] = self.getLabels(labels)
+
+        return item
     
+    def getLabels(self, labels):
+        labelsInt = [int(label) for label in labels]
+ 
+        return torch.tensor(labelsInt)
+
+    def getData(self, raw):
+        data = []
+        for item in raw:
+            itemShape = len(np.shape(item))
+            if (itemShape == 1):
+                for part in raw:
+                    data.append(part)
+            else:
+                data.append(item)
+
+        return data
+
+    def dataToTensor(self, data):
+        self.bertTknzr.reset()
+        lines = self.getLines(data)
+        tokensTensor = self.bertTknzr.tokenize(lines)
+        
+        return tokensTensor
+
+    def getLines(self, data):
+        linesFlattened = []
+        for item in data:
+            if type(item) == str:
+                lines  = item.split('\n')
+            else:
+                lines = item[0].split('\n')
+
+            for line in lines:
+                linesFlattened.append(line)
+            
+        return linesFlattened;
+        
+    def filterData(self, dataRaw):
+        # res, per ara.
+        
+        return dataRaw
+
+if __name__ == '__main__':
+    ds = EURLEX57KDataset('FilesIndexShort.txt')
+    for i in range(5):
+        item = ds.__getitem__(i)
+        print(i, 'item["data"]: ', item["data"].shape)
+        print(i, 'item["labels"]: ', item["labels"])
+        print('data: ', item['data'][0:10])
