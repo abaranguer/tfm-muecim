6a02c7c81dda27a1da93bf378d1349f8470926dd
Albert Baranguer i Codina <abaranguer@gmail.com>
2025-02-07 21:50:28 +0000

    scripts shell d'inicialiotzació

---
+++ git_globals.sh
@@ -0,0 +1,6 @@
+echo "Set git global properties"
+git config --global user.email "abaranguer@gmail.com"
+git config --global user.name "Albert Baranguer i Codina"
+echo
+echo "Done!"
+
---
+++ install_sw.sh
@@ -0,0 +1,9 @@
+echo
+echo "Install emacs"
+apt install emacs
+echo
+echo "Install joe"
+apt install joe
+echo
+echo "Done!"
+
