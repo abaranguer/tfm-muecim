12dafcdb3fa50a0eea84eabe613d5e0c0c318f0c
Albert Baranguer i Codina <abaranguer@gmail.com>
2025-05-05 21:13:35 +0000

    notebooks

---
+++ sh/tfm_www_copy.sh
@@ -0,0 +1,4 @@
+cp -r /content/tfm_www/classifiers /content/drive/MyDrive/TFM-MUECIM/tfm_www
+cp -r /content/tfm_www/demo-flask-app.py /content/drive/MyDrive/TFM-MUECIM/tfm_www
+cp -r /content/tfm_www/static /content/drive/MyDrive/TFM-MUECIM/tfm_www 
+cp -r /content/tfm_www/summarizers /content/drive/MyDrive/TFM-MUECIM/tfm_www 
